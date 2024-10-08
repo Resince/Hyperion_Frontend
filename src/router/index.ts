2a1325fb8f802d1f46306118e57b200a33cf0b21
ushio <986052589@qq.com>
2024-10-09 00:08:22 +0800

    add some mainscc

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,38 +1,37 @@
-import { createRouter, createWebHistory } from "vue-router";
-import routes from "./router";
+import {createRouter, createWebHistory} from 'vue-router';
+
+import routes from './router';
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
-    scrollBehavior(_to, _from, _savedPosition) {
-        // 始终滚动到顶部
-        return { top: 0 };
-    },
+  history: createWebHistory(),
+  routes,
+  scrollBehavior(_to, _from, _savedPosition) {
+    // 始终滚动到顶部
+    return {top: 0};
+  },
 });
 
 // 路由守卫，让没有登录的登录
 router.beforeEach(async (to, _from) => {
-    const isAuthenticated = true;
-    const role = "CONSUMER";
+  const isAuthenticated = true;
+  const role = 'ADMIN';
 
-    if (
-        // 需要鉴权的页面
-        to.meta.role &&
-        // 但是没有登录
-        !isAuthenticated &&
-        // 避免无限重定向
-        to.path != "/login"
-    ) {
-        return { name: "LoginRegister" };
-    } else if (
-        // 登录鉴权
-        // 需要鉴权
-        to.meta.role &&
-        // 没有权限
-        role != to.meta.role
-    ) {
-        return { name: "404" };
-    }
+  if (
+      // 需要鉴权的页面
+      to.meta.role &&
+      // 但是没有登录
+      !isAuthenticated &&
+      // 避免无限重定向
+      to.path != '/login') {
+    return {name: 'LoginRegister'};
+  } else if (
+      // 登录鉴权
+      // 需要鉴权
+      to.meta.role &&
+      // 没有权限
+      role != to.meta.role) {
+    return {name: '404'};
+  }
 });
 
 export default router;
--- src/router/router.ts
+++ src/router/router.ts
@@ -1,117 +1,111 @@
-import LoginRegister from "@/views/login/index.vue";
-import ConsumerLayout from "@/layouts/consumerLayout/index.vue";
-import ConsumerHome from "@/views/consumer/home/index.vue";
+import ConsumerLayout from '@/layouts/consumerLayout/index.vue';
+import ConsumerHome from '@/views/consumer/home/index.vue';
+import LoginRegister from '@/views/login/index.vue';
 
 const routes = [
-    {
-        path: "/test",
-        name: "test",
-        component: () => import("@/views/consumer/home/components/card.vue"),
+  {
+    path: '/test',
+    name: 'test',
+    component: () => import('@/views/consumer/home/components/card.vue'),
+  },
+  {
+    path: '/',
+    redirect: '/login',
+  },
+  {
+    path: '/login',
+    name: 'LoginRegister',
+    component: LoginRegister,
+  },
+  {
+    path: '/consumer',
+    name: 'ConsumerLayout1',
+    component: ConsumerLayout,
+    meta: {
+      role: 'CONSUMER',
     },
-    {
-        path: "/",
-        redirect: "/login",
+    children: [
+      {
+        path: 'home/:category',
+        name: 'ConsumerHome',
+        component: ConsumerHome,
+      },
+      {
+        path: 'shoppinglist',
+        name: 'ShoppingList',
+        component: () => import('@/views/consumer/shoppingList/index.vue'),
+      },
+      {
+        path: 'profile',
+        name: 'Profile',
+        component: () => import('@/views/consumer/profile/index.vue'),
+      },
+      {
+        path: 'goodsdetails/:id',
+        name: 'GoodsDetails',
+        component: () => import('@/views/consumer/goodsDetails/index.vue'),
+      },
+    ],
+  },
+  {
+    path: '/merchant',
+    name: 'ManagerLayout1',
+    component: () => import('@/layouts/managerLayout/index.vue'),
+    meta: {
+      role: 'MERCHANT',
     },
-    {
-        path: "/login",
-        name: "LoginRegister",
-        component: LoginRegister,
+    children: [
+      {
+        path: '',
+        name: 'MerchantHome',
+        component: () => import('@/views/merchant/home/index.vue'),
+      },
+      {
+        path: 'commodityManage',
+        name: 'CommodityManage',
+        component: () => import('@/views/merchant/commodityManage/index.vue'),
+      },
+      {
+        path: 'orderManage',
+        name: 'OrderNanage',
+        component: () => import('@/views/merchant/orderManage/index.vue'),
+      },
+      {
+        path: 'orderdetails',
+        name: 'OrderDetails',
+        component: () => import('@/views/merchant/orderDetails/index.vue'),
+      },
+    ],
+  },
+  {
+    path: '/admin',
+    name: 'ManagerLayout',
+    component: () => import('@/layouts/managerLayout/index.vue'),
+    meta: {
+      role: 'ADMIN',
     },
-    {
-        path: "/consumer",
-        name: "ConsumerLayout",
-        component: ConsumerLayout,
-        meta: {
-            role: "CONSUMER",
-        },
-        children: [
-            {
-                path: "home/:category",
-                name: "ConsumerHome",
-                component: ConsumerHome,
-            },
-            {
-                path: "shoppinglist",
-                name: "ShoppingList",
-                component: () =>
-                    import("@/views/consumer/shoppingList/index.vue"),
-            },
-            {
-                path: "profile",
-                name: "Profile",
-                component: () => import("@/views/consumer/profile/index.vue"),
-            },
-            {
-                path: "goodsdetails/:id",
-                name: "GoodsDetails",
-                component: () =>
-                    import("@/views/consumer/goodsDetails/index.vue"),
-            },
-        ],
-    },
-    {
-        path: "/merchant",
-        name: "ManagerLayout",
-        component: () => import("@/layouts/managerLayout/index.vue"),
-        meta: {
-            role: "MERCHANT",
-        },
-        children: [
-            {
-                path: "",
-                name: "MerchantHome",
-                component: () => import("@/views/merchant/home/index.vue"),
-            },
-            {
-                path: "commodityManage",
-                name: "CommodityManage",
-                component: () =>
-                    import("@/views/merchant/commodityManage/index.vue"),
-            },
-            {
-                path: "orderManage",
-                name: "OrderNanage",
-                component: () =>
-                    import("@/views/merchant/orderManage/index.vue"),
-            },
-            {
-                path: "orderdetails",
-                name: "OrderDetails",
-                component: () =>
-                    import("@/views/merchant/orderDetails/index.vue"),
-            },
-        ],
-    },
-    {
-        path: "/admin",
-        name: "ManagerLayout",
-        component: () => import("@/layouts/managerLayout/index.vue"),
-        meta: {
-            role: "ADMIN",
-        },
-        children: [
-            {
-                path: "merchantmanage",
-                name: "MerchantManage",
-                component: () =>
-                    import("@/views/admin/merchantManage/index.vue"),
-            },
-            {
-                path: "usermanage",
-                name: "UserManage",
-                component: () => import("@/views/admin/userManage/index.vue"),
-            },
-        ],
-    },
-    {
-        path: "/:pathMatch(.*)*",
-        redirect: "/404",
-    },
-    {
-        path: "/404",
-        name: "404",
-        component: () => import("@/views/404/index.vue"),
-    },
+    children: [
+      {
+        path: 'merchantmanage',
+        name: 'MerchantManage',
+        component: () => import('@/views/admin/merchantManage/index.vue'),
+      },
+      {
+        path: 'usermanage',
+        name: 'UserManage',
+        component: () => import('@/views/admin/userManage/index.vue'),
+      },
+    ],
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: '/404',
+  },
+  {
+    path: '/404',
+    name: '404',
+    component: () => import('@/views/404/index.vue'),
+  },
 ];
 
 export default routes;
